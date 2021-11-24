e3527881999ca837b4bb6a83ae3f24cc11143c9e
SeBzH29 <sebzh29@outlook.com>
2021-11-24 09:23:40 +0100

    ajout de relation film/actor

--- src/Controller/ActorController.php
+++ src/Controller/ActorController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Actor;
+use App\Form\ActorType;
 use App\Repository\ActorRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,12 +16,50 @@ class ActorController extends AbstractController
     /**
      * @Route("/actor", name="actor")
      */
-    public function index(ActorRepository $repository): Response
+    public function index(ActorRepository $ar): Response
     {
         // $repository = $this->getDoctrine()->getRepository(Actor::class);
-        $actors = $repository->findAll();
+        $actors = $ar->findAll();
         return $this->render('actor/index.html.twig', [
             'actors' => $actors
+        ]);
+    }
+
+    /**
+     * @Route("/afficheactor/{id}", name="afficheactor")
+     */
+
+    // public function afficherUnActeur(ActorRepository $ar, $id): Response 
+    public function afficherUnActeur(Actor $actor): Response 
+    {
+        //
+        // $actor = $ar->find($id);
+        return $this->render('actor/afficherUn.html.twig',["actor"=>$actor]);
+    }
+
+    /**
+     * @Route("/modifactor/{id}", name="modifactor")
+     * @Route("/creationactor", name="creationactor")
+     */
+
+    public function modifierActor(Actor $actor=null, Request $request, EntityManagerInterface $em) {
+
+        if (!$actor) {
+            $actor = new Actor();
+        }
+
+        $form = $this->createForm(ActorType::class,$actor);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() ) {
+            $em->persist($actor);
+            $em->flush();
+            return $this->redirectToRoute('actor');
+        }
+
+        return $this->render('actor/modifActor.html.twig',[
+            "actor"=>$actor,
+            "form"=>$form->createView()
         ]);
     }
 }
---
+++ src/Form/ActorType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Actor;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ActorType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('photo')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Actor::class,
+        ]);
+    }
+}
